f65dae48106cdb60c9463dfd47f2e8beb5221e5c
kroica <krkregistros@gmail.com>
2021-04-22 10:42:23 -0300

    Novas palavras para Entre Linhas.

--- entre_linhas/script.js
+++ entre_linhas/script.js
@@ -1,5 +1,5 @@
 
-const WORDS_BASE = ["Urso", "Bigode", "Férias", "Violão", "Laranja", "Montanha", "Médico", "Rápido", "Alegria"]
+const WORDS_BASE = ["Urso", "Bigode", "Férias", "Violão", "Laranja", "Montanha", "Médico", "Rápido", "Alegria", "Chafariz", "Bola", "Feijoada", "Pantanal", "Noite", "Pesado", "Gato", "Amazônia", "Paçoca", "Cafuné", "Jogo de Tabuleiro", "Jogo de Cartas", "Saudade", "Pão de Queijo", "Relâmpago", "Flecha", "Viagem", "Dragão", "Carro", "Estranho", "Refeição", "Avião", "Trailer", "Veterinário", "Tubarão", "Notebook", "Mamadeira", "Bombom", "Pato", "Camelo", "Medo", "Cinza", "Óculos", "Cobra", "Estrada", "Bombeiro", "Palmeira", "Plástico", "Elástico", "Trigo", "Branco", "Circo", "Chocolate"]
 let words = ["um", "dois", "tres", "quatro"]
 let labels = ["A", "1", "B", "2", "C", "3"]
 
